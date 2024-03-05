40f45f12ce50434e1a92aedb09ae4a60673011da
Evgenii <uvaz2014@mail.ru>
2024-03-06 00:46:40 +0300

    try batching

--- yolo_video.py
+++ yolo_video.py
@@ -87,6 +87,82 @@ class SimpleTracker:
         return self.tracks
 
 
+def process_batch(model, frames, tracker, class_names, track_histories):
+    batch_detections = model(frames, stream=True, classes=list(class_names.keys()), verbose=False)
+
+    for i, results in enumerate(batch_detections):
+        # print(i, results)
+        img = frames[i]
+        detections = []
+        for box in results.boxes:
+            x1, y1, x2, y2 = box.xyxy
+            data = self.boxes.data.cpu().tolist()
+            # x1, y1, x2, y2, conf, cls_id = map(int, box[:4]) + [box[4], int(box[5])]
+            # detections.append((x1, y1, x2, y2, conf, cls_id))
+    #     tracks = tracker.update(detections)
+    #     print("P2")
+    #     # Draw the tracks
+    #     for track in tracks:
+    #         x1, y1, x2, y2 = track['x1'], track['y1'], track['x2'], track['y2']
+    #         obj_id = track['id']
+    #         cls_id = track['cls_id']
+    #         conf = track['conf']
+    #
+    #         # Calculate the center coordinates of the bounding box
+    #         cx = (x1 + x2) // 2
+    #         cy = (y1 + y2) // 2
+    #
+    #         # Update history of track
+    #         track_histories[obj_id].append({'cx': cx, 'cy': cy, 'current_frames': current_frames})
+    #
+    #         # Draw a circle at the center and display the object ID
+    #         cv2.circle(img, (cx, cy), 5, (0, 255, 0), -1)
+    #
+    #         # Calculate dynamic font size based on bounding box size
+    #         box_width = x2 - x1
+    #         box_height = y2 - y1
+    #         avg_box_size = (box_width + box_height) / 2
+    #         font_scale = max(0.4, avg_box_size / 250)  # Adjust the denominator to control the scaling factor
+    #
+    #         # Display object ID with dynamic font size
+    #         cv2.putText(img, f"ID: {obj_id}", (cx - 10, cy - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255),
+    #                     2)
+    #
+    #         # Draw bounding box
+    #         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 140, 255), 3)
+    #
+    #         # Get confidence and class name
+    #         confidence_rounded = math.ceil(track['conf'] * 100) / 100
+    #         class_name = class_names[cls_id]
+    #
+    #         font_thickness = max(1, int(font_scale * 2))
+    #         text = f"{class_name}, {confidence_rounded}"
+    #         # Get text size
+    #         (text_width, text_height), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_thickness)
+    #
+    #         # Set background rectangle coordinates
+    #         background_tl = x1, y1 - text_height - 3
+    #         background_br = x1 + text_width, y1
+    #
+    #         # Draw filled rectangle for text background
+    #         cv2.rectangle(img, background_tl, background_br, (0, 140, 255), -1)  # Orange background
+    #
+    #         # Draw text
+    #         text_position = x1, y1 - 2
+    #         cv2.putText(img, text, text_position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255), font_thickness)
+    #     print("P3")
+    #     # Draw tracking line
+    #     for obj_id in track_histories:
+    #         if len(track_histories[obj_id]) > 0 and current_frames - track_histories[obj_id][0]['current_frames'] >= 50:
+    #             track_histories[obj_id].popleft()
+    #
+    #         for i in range(1, len(track_histories[obj_id])):
+    #             start_point = (track_histories[obj_id][i - 1]['cx'], track_histories[obj_id][i - 1]['cy'])
+    #             end_point = (track_histories[obj_id][i]['cx'], track_histories[obj_id][i]['cy'])
+    #             cv2.line(img, start_point, end_point, (0, 255, 0), 2)
+    #     print(frames)
+    #     out.write(img)
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -113,102 +189,29 @@ if __name__ == "__main__":
 
     # Load model
     model = YOLO("models/yolov8n.pt")
-
-    # Initialize the simple tracker
     tracker = SimpleTracker()
-
     progress_bar = tqdm(total=total_frames, desc='Processing Frames', unit='frames')
+    track_histories = defaultdict(lambda: deque(maxlen=50))
 
-    track_histories = defaultdict(lambda: deque(maxlen=50))  # Store up to 50 points for each track
-
+    batch_size = 4  # Define your batch size here
+    frames_batch = []
     current_frames = 0
-    print("_"*10, frame_rate)
     while True:
         success, img = cap.read()
-        if not success:
-            break  # Exit if the video ends or there are no frames left
+        if not success and len(frames_batch) == 0:
+            break  # Exit if the video ends and no frames are left in the batch
+        if success:
+            frames_batch.append(img)
+            if len(frames_batch) < batch_size and success:
+                continue  # Collect enough frames for a batch
 
-        current_frames += 1
-        results = model(img, stream=True, classes=list(class_names.keys()), verbose=False)
-
-        # Extract detections
-        detections = []
-        for r in results:
-            boxes = r.boxes
-            for box in boxes:
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                conf = box.conf
-                cls_id = int(box.cls[0])  # Get the class ID
-                detections.append((x1, y1, x2, y2, conf, cls_id))
-
-        # Update the tracker with the new detections
-        tracks = tracker.update(detections)
-
-        # Draw the tracks
-        for track in tracks:
-            x1, y1, x2, y2 = track['x1'], track['y1'], track['x2'], track['y2']
-            obj_id = track['id']
-            cls_id = track['cls_id']
-            conf = track['conf']
-
-            # Calculate the center coordinates of the bounding box
-            cx = (x1 + x2) // 2
-            cy = (y1 + y2) // 2
-
-            # Update history of track
-            track_histories[obj_id].append({'cx': cx, 'cy': cy, 'current_frames': current_frames})
-
-            # Draw a circle at the center and display the object ID
-            cv2.circle(img, (cx, cy), 5, (0, 255, 0), -1)
-
-            # Calculate dynamic font size based on bounding box size
-            box_width = x2 - x1
-            box_height = y2 - y1
-            avg_box_size = (box_width + box_height) / 2
-            font_scale = max(0.4, avg_box_size / 250)  # Adjust the denominator to control the scaling factor
-
-            # Display object ID with dynamic font size
-            cv2.putText(img, f"ID: {obj_id}", (cx - 10, cy - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255),
-                        2)
-
-            # Draw bounding box
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 140, 255), 3)
-
-            # Get confidence and class name
-            confidence_rounded = math.ceil(track['conf'] * 100) / 100
-            class_name = class_names[cls_id]
-
-            font_thickness = max(1, int(font_scale * 2))
-            text = f"{class_name}, {confidence_rounded}"
-            # Get text size
-            (text_width, text_height), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_thickness)
-
-            # Set background rectangle coordinates
-            background_tl = x1, y1 - text_height - 3
-            background_br = x1 + text_width, y1
-
-            # Draw filled rectangle for text background
-            cv2.rectangle(img, background_tl, background_br, (0, 140, 255), -1)  # Orange background
-
-            # Draw text
-            text_position = x1, y1 - 2
-            cv2.putText(img, text, text_position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255), font_thickness)
-
-        # Draw tracking line
-        for obj_id in track_histories:
-            if len(track_histories[obj_id]) > 0 and current_frames - track_histories[obj_id][0]['current_frames'] >= 50:
-                track_histories[obj_id].popleft()
-
-            for i in range(1, len(track_histories[obj_id])):
-                start_point = (track_histories[obj_id][i - 1]['cx'], track_histories[obj_id][i - 1]['cy'])
-                end_point = (track_histories[obj_id][i]['cx'], track_histories[obj_id][i]['cy'])
-                cv2.line(img, start_point, end_point, (0, 255, 0), 2)
-
-        progress_bar.update(1)
-        out.write(img)
-        cv2.imshow("Tracking", img)
-        if cv2.waitKey(1) == ord('q'):
-            break
+        # Process the batch
+        process_batch(model, frames_batch, tracker, class_names, track_histories)
+        print("P3")
+        current_frames += len(frames_batch)
+        print(current_frames)
+        progress_bar.update(len(frames_batch))
+        frames_batch = []  # Clear the batch
 
     cap.release()
     cv2.destroyAllWindows()
